4f6d468416873c1cfa561398c91d96a96743f39f
dylancatala <dylan.catala@proton.me>
2024-02-08 11:52:05 +0100

    Adding tasks notifications

--- script.js
+++ script.js
@@ -12,6 +12,16 @@ function uuidv4() {
     });
 }
 
+
+function randomClassNameTask() {
+  return 'task-xxx'
+    .replace(/[x]/g, function (c) {
+      const r = Math.random() * 3 | 0,
+        v = c == 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(3);
+    });
+}
+
 let taskList = [{
   id: uuidv4(),
   name: "Tâche 1"
@@ -23,10 +33,17 @@ let taskList = [{
 getSaveButton.addEventListener("click", () => {
   taskList.push({ id: uuidv4(), name: getTaskNameInput.value })
   localStorage.setItem('TaskList', JSON.stringify(taskList))
+  displayTask()
 });
 
 
 
 function displayTask() {
-
+  const getTaskDiv = document.getElementById("addTask")
+  const createElement = document.createElement("p");
+  taskList.forEach(function (taskItem) {
+    createElement.innerText = taskItem.name
+  })
+  createElement.classList.add(randomClassNameTask())
+  getTaskDiv.appendChild(createElement)
 }
